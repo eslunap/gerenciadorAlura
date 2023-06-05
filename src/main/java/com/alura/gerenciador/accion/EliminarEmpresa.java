dcb47b4ba35c41e47f8a625622a24b4f6ba58cf1
eslunap <estefany.luna@gmail.com>
2023-06-04 23:12:56 -0500

    Update EliminarEmpresa.java
    updtate metodo con retorno

--- src/main/java/com/alura/gerenciador/accion/EliminarEmpresa.java
+++ src/main/java/com/alura/gerenciador/accion/EliminarEmpresa.java
@@ -9,7 +9,7 @@ import jakarta.servlet.http.HttpServletResponse;
 
 public class EliminarEmpresa {
 
-	public void ejecutar(HttpServletRequest request, HttpServletResponse response) throws IOException {
+	public String ejecutar(HttpServletRequest request, HttpServletResponse response) throws IOException {
 			
 		String paramId = request.getParameter("id");
 		Integer id = Integer.valueOf(paramId);
@@ -21,8 +21,7 @@ public class EliminarEmpresa {
 		DB db = new DB();
 		db.eliminarEmpresa(id);
 		
-		response.sendRedirect("entrada?accion=ListaEmpresas");
-		
+		return "redirect:entrada?accion=ListaEmpresas";		
 	}
 
 }
