b68d4dbb6d2736b60099ef2950b66c5068ad1bba
eslunap <estefany.luna@gmail.com>
2023-06-04 21:57:36 -0500

    Update EliminarEmpresa.java
    se agrega throws IOException

--- src/main/java/com/alura/gerenciador/accion/EliminarEmpresa.java
+++ src/main/java/com/alura/gerenciador/accion/EliminarEmpresa.java
@@ -1,5 +1,27 @@
 package com.alura.gerenciador.accion;
 
+import java.io.IOException;
+
+import com.alura.gerenciador.modelo.DB;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
 public class EliminarEmpresa {
+
+	public void ejecutar(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		System.out.println("Eliminando una empresa");
+		
+		String paramId = request.getParameter("id");
+		Integer id = Integer.valueOf(paramId);
+		
+		System.out.println(id);
+		
+		DB db = new DB();
+		db.eliminarEmpresa(id);
+		
+		response.sendRedirect("listaEmpresas");
+		
+	}
 
 }
